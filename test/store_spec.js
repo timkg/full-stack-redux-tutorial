017c534d5c4975099f05ec769d1d6a7985ed7210
timkg <tim.theodor@gmail.com>
2015-10-20 03:11:49 +0200

    adds redux store

---
+++ test/store_spec.js
@@ -0,0 +1,19 @@
+import { fromJS } from "immutable";
+import { expect } from "chai";
+
+import makeStore from "../src/store";
+
+describe("store", () => {
+  it("is a Redux store configured with the correct reducer", () => {
+    const store = makeStore();
+    expect(store.getState()).to.equal(fromJS({}));
+
+    store.dispatch({
+      type: "SET_ENTRIES",
+      entries: ["Trainspotting", "Sunshine"]
+    });
+    expect(store.getState()).to.equal(fromJS({
+      entries: ["Trainspotting", "Sunshine"]
+    }));
+  });
+});
